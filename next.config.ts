eae8675904f86247e4f4dfd8e5176da557d31caa
nicolasmichielon <nicolasmichielon@gmail.com>
2025-05-18 11:59:04 -0300

    feat: add ESLint configuration to ignore errors during builds

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,13 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ['cdn.builder.io'],
   },
 };
+
 
 export default nextConfig;
